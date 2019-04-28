f9a18fa36851f8346e572319beb6ffe13f5405cc
Thomas Liu <liuxk99@gmail.com>
2019-04-28 16:05:29 +0800

    <Development>: add app template

---
+++ Android.mk
@@ -0,0 +1,29 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+res_dirs := res
+src_dirs := src
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := $(call all-java-files-under,$(src_dirs))
+
+# unbundled
+LOCAL_STATIC_JAVA_LIBRARIES += android-support-v4
+
+LOCAL_SDK_VERSION := current
+
+LOCAL_RESOURCE_DIR := $(addprefix $(LOCAL_PATH)/, $(res_dirs))
+
+LOCAL_PACKAGE_NAME := Hello
+
+LOCAL_PROGUARD_FLAG_FILES := proguard-project.txt
+
+ifneq ($(filter letv_jason%, $(TARGET_PRODUCT)),)
+LOCAL_VENDOR_MODULE := true
+endif
+
+include $(BUILD_PACKAGE)
+
+# Use the following include to make our test apk.
+include $(call all-makefiles-under,$(LOCAL_PATH))
