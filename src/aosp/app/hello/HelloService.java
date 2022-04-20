4c3c8f2f646cf4a072cdd3a74e4a43ed8a201456
Thomas Liu <liuxk99@gmail.com>
2022-04-20 19:09:06 +0800

    [Dev]<#F-feature>: Time API on uptime and current time.

--- src/aosp/app/hello/HelloService.java
+++ src/aosp/app/hello/HelloService.java
@@ -4,6 +4,8 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.os.SystemClock;
+import android.util.Log;
 
 import com.sj4a.utils.SjLog;
 import com.sj4a.utils.SjLogGen;
@@ -16,6 +18,7 @@ public class HelloService extends Service {
     SjLogGen mLogGen = new SjLogGen(TAG);
 
     static final String ACTION_INIT = "aosp.app.hello.intent.action.INIT";
+    private static final String ACTION_UPTIME = "aosp.app.hello.intent.action.UPTIME";
 
     @Override
     public void onCreate() {
@@ -36,12 +39,47 @@ public class HelloService extends Service {
             final String a = intent.getAction();
             if (ACTION_INIT.equals(a)) {
                 actionInit(intent.getExtras());
+            } else if (ACTION_UPTIME.equals(a)) {
+                actionUptime(intent.getExtras());
             }
+            
             res = super.onStartCommand(intent, flags, startId);
         }
         sjLog.out();
 
         return res;
+    }
+
+    private void actionUptime(Bundle extras) {
+        SjLog sjLog = mLogGen.build("actionUptime(" + extras + ")");
+        sjLog.in();
+        {
+            Log.i(TAG, "uptime①:" + SystemClock.uptimeMillis());
+            Log.i(TAG, "currentTime①:" + System.currentTimeMillis());
+            {
+                //{{ 回拨2S
+                //{{ set current time 2 seconds backward.
+                long currentTime = System.currentTimeMillis() - 2000;
+                boolean res = SystemClock.setCurrentTimeMillis(currentTime);
+                Log.i(TAG, res + " = setCurrentTimeMillis(" + currentTime + ")");
+                if (res) {
+                    Log.i(TAG, "currentTime:" + System.currentTimeMillis());
+                }
+                //}}
+
+                try {
+                    // sleep 0.5s
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            // the uptime② is 0.5s more than uptime①
+            Log.i(TAG, "uptime②:" + SystemClock.uptimeMillis());
+            // the current time② is 1.5s less than current time①
+            Log.i(TAG, "currentTime②:" + System.currentTimeMillis());
+        }
+        sjLog.out();
     }
 
     private void actionInit(Bundle extras) {
